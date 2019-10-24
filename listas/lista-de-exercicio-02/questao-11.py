3d85d34b41907301b3bf68a65e35ca4acfe8a2d9
Laise Santos <layse0877@gmail.com>
2019-10-24 16:44:41 -0300

    Implemetando a questão 11

---
+++ listas/lista-de-exercicio-02/questao-11.py
@@ -0,0 +1,11 @@
+numero1 = int(input("digite um numero inteiro: "))
+numero2 = int(input("digite outro numero inteiro: "))
+numero3 = float(input("digite um numero real: "))
+
+multiplicacao = (numero1*2)*(numero2/2)
+soma = (numero1*3)+ numero3
+exponenciao = numero3**3
+
+print("o produto é: {}".format(multiplicacao))
+print("a soma é: {:.2f}".format(soma))
+print("o cubo é: {:.2f}".format(exponenciao))
