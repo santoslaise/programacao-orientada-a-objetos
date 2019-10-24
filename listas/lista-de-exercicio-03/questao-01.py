10609d2257d9a28595b5a8931ec34fc8fbf5fbac
Laise santos <layse0877@gmail.com>
2019-10-24 20:58:03 -0300

    implemetando a primeira questão de 3 lista

---
+++ listas/lista-de-exercicio-03/questao-01.py
@@ -0,0 +1,6 @@
+numero = int(input("digite um numero: "))
+numero2 = int(input("digite outro numero: "))
+if(numero>numero2):
+  print("{} é o maior".format(numero))
+else:
+  print("{} é o maior".format(numero2))
