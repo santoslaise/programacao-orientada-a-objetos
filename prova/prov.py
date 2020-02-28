3be3570c0177f74b9912d22f76ec5b0fe000abda
santoslaise <56684181+santoslaise@users.noreply.github.com>
2020-02-28 15:06:58 -0300

    prova

---
+++ prova/prov.py
@@ -0,0 +1,75 @@
+'''from datetime import datetime'''
+
+class Pessoa:
+    
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def __str__(self):
+        
+
+		
+class Vendedor:
+    
+    def __init__(self, salario):
+        
+        super(Pessoa(nome,idade)
+              
+              #self.nome = nome
+              #self.idade = idade
+              self.salario = salario
+              print(f"{self.nome},{self.idade},{self.salario}")
+        
+        
+class Cliente:
+    def __init__(self,compra = []):
+        super(Pessoa(nome,idade).__init__()
+              self.compra = compra = []
+              print(f"{self.compra},{self.nome},{self.idade}")
+        
+        
+
+        
+    def registrar_conta(self,compra):
+        registrar = compra
+        if(registrar == True):
+             self.compra.append(registrar)
+             
+    def total_de_compras(self):
+        super().Compra(valor)
+        self.valor = self.valor+valor
+
+    def get_data_ultima_compra(self):
+        print(
+        
+        
+        
+
+class Compra:
+    def __init__(self, vendedor, data, valor):
+        self.vendedor = vendedor
+        self.data = datetime.now()
+        self.valor = valor
+
+
+def main():
+    p1 = Pessoa('luiza',23)
+    p2 = Pessoa('tae',22)
+    print(f"{self.nome},{self.idade}")
+
+    v1 = Vendedor(1000)
+    cliente = Cliente()
+    cliente.registrar_conta(200)
+    cliente.total_de_compras()
+
+    compra = Compra('tae',datetime.now(),300)
+    compra.vededor()
+    compra.data(datetime)
+    
+
+
+
+        
+    
+	    
